58f632a12c0556fc43518517fd2c6000971964e7
unknown <Tangilur Rahman>
2022-09-25 10:36:27 +0600

    set-up again

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 // external components
 import { useState } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
