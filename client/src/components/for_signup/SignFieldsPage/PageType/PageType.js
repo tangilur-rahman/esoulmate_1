0862782ef2e093f8aae3b82913b79b14d1c4952e
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2022-10-05 11:07:18 +0600

    signup-page ui done

---
+++ client/src/components/for_signup/SignFieldsPage/PageType/PageType.js
@@ -0,0 +1,173 @@
+// external components
+import { useState } from "react";
+
+// internal components
+import "./PageType.css";
+
+const PageType = ({ getPType, setPType }) => {
+	// for drop-down toggle
+	const [typeDrop, setTypeDrop] = useState("");
+
+	return (
+		<>
+			<div
+				className={typeDrop ? "type-container active" : "type-container"}
+				onClick={() => setTypeDrop(!typeDrop)}
+			>
+				<input
+					type="text"
+					placeholder="Select Page Type"
+					readOnly
+					value={getPType}
+				/>
+				<div className="option">
+					<div onClick={() => setPType("animator")}>
+						<span>Animator</span>
+					</div>
+
+					<div onClick={() => setPType("architect")}>
+						<span>Architect</span>
+					</div>
+
+					<div onClick={() => setPType("artist")}>
+						<span>Artist</span>
+					</div>
+
+					<div onClick={() => setPType("baker")}>
+						<span>Baker</span>
+					</div>
+
+					<div onClick={() => setPType("colorist")}>
+						<span>Colorist</span>
+					</div>
+
+					<div onClick={() => setPType("comedian")}>
+						<span>Comedian</span>
+					</div>
+
+					<div onClick={() => setPType("content writer")}>
+						<span>Content Writer</span>
+					</div>
+
+					<div onClick={() => setPType("cook")}>
+						<span>Cook</span>
+					</div>
+
+					<div onClick={() => setPType("curator")}>
+						<span>Curator</span>
+					</div>
+
+					<div onClick={() => setPType("dancer")}>
+						<span>Dancer</span>
+					</div>
+
+					<div onClick={() => setPType("designer")}>
+						<span>Designer</span>
+					</div>
+
+					<div onClick={() => setPType("digital artist")}>
+						<span>Digital Artist</span>
+					</div>
+
+					<div onClick={() => setPType("essayist")}>
+						<span>Essayist</span>
+					</div>
+
+					<div onClick={() => setPType("event planner")}>
+						<span>Event Planner</span>
+					</div>
+
+					<div onClick={() => setPType("fashion designer")}>
+						<span>Fashion Designer</span>
+					</div>
+
+					<div onClick={() => setPType("fine artist")}>
+						<span>Fine Artist</span>
+					</div>
+
+					<div onClick={() => setPType("floral designer")}>
+						<span>Floral Designer</span>
+					</div>
+
+					<div onClick={() => setPType("graphic designer")}>
+						<span>Graphic Designer</span>
+					</div>
+
+					<div onClick={() => setPType("hairstylist")}>
+						<span>Hairstylist</span>
+					</div>
+
+					<div onClick={() => setPType("illustrator")}>
+						<span>Illustrator</span>
+					</div>
+
+					<div onClick={() => setPType("interior designer")}>
+						<span>Interior Designer</span>
+					</div>
+
+					<div onClick={() => setPType("jewelry designer")}>
+						<span>Jewelry Designer</span>
+					</div>
+
+					<div onClick={() => setPType("lyricist")}>
+						<span>Lyricist</span>
+					</div>
+
+					<div onClick={() => setPType("make-up artist")}>
+						<span>Make-up Artist</span>
+					</div>
+
+					<div onClick={() => setPType("marine designer")}>
+						<span>Marine Designer</span>
+					</div>
+
+					<div onClick={() => setPType("media designer")}>
+						<span>Media Designer</span>
+					</div>
+
+					<div onClick={() => setPType("musician")}>
+						<span>Musician</span>
+					</div>
+
+					<div onClick={() => setPType("painter")}>
+						<span>Painter</span>
+					</div>
+
+					<div onClick={() => setPType("penciler")}>
+						<span>Penciler</span>
+					</div>
+
+					<div onClick={() => setPType("photographer")}>
+						<span>Photographer</span>
+					</div>
+
+					<div onClick={() => setPType("photojournalist")}>
+						<span>Photojournalist</span>
+					</div>
+
+					<div onClick={() => setPType("potter")}>
+						<span>Potter</span>
+					</div>
+
+					<div onClick={() => setPType("singer")}>
+						<span>Singer</span>
+					</div>
+
+					<div onClick={() => setPType("web designer")}>
+						<span>Web Designer</span>
+					</div>
+
+					<div onClick={() => setPType("wedding planner")}>
+						<span>Wedding Planner</span>
+					</div>
+
+					<div onClick={() => setPType("writer")}>
+						<span>Writer</span>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default PageType;
--- client/src/components/for_signup/SignFieldsPage/SignFieldsPage.js
+++ client/src/components/for_signup/SignFieldsPage/SignFieldsPage.js
@@ -1,10 +1,120 @@
 // external components
+import { useState } from "react";
 
 // internal components
+import PageType from "./PageType/PageType";
 import "./SignFieldsPage.css";
 
 const SignFieldsPage = () => {
-	return <div>SignFieldsPage</div>;
+	// for getting page-name
+	const [getPName, setPName] = useState("");
+
+	// for getting email_phone
+	const [email_phone, setEmail_phone] = useState("");
+
+	// for get page-type
+	const [getPType, setPType] = useState("");
+
+	// for getting password
+	const [password, setPassword] = useState("");
+
+	// for getting c_password
+	const [c_password, setC_Password] = useState("");
+
+	// for toggle password type
+	const [typeP_T, setTypeP_T] = useState(false);
+	const [typeCP_T, setTypeCP_T] = useState(false);
+
+	return (
+		<>
+			<div className="sign-page-container">
+				<table>
+					<tr className="required-row">
+						<input
+							type="text"
+							required
+							placeholder="Page Name..."
+							value={getPName}
+							onChange={(e) => setPName(e.target.value)}
+							autoComplete="off"
+						/>
+					</tr>
+
+					<tr>
+						<PageType getPType={getPType} setPType={setPType} />
+					</tr>
+
+					<tr className="required-row">
+						<input
+							type="text"
+							required
+							placeholder="Email or Phone No..."
+							value={email_phone}
+							onChange={(e) => setEmail_phone(e.target.value)}
+						/>
+					</tr>
+
+					<tr className="required-row">
+						<input
+							type={typeP_T ? "text" : "password"}
+							required
+							placeholder="New Password"
+							name="password"
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+						{/* for type toggle start  */}
+						{password && (
+							<span className="eye">
+								{typeP_T ? (
+									<i
+										className="fa-solid fa-eye"
+										onClick={() => setTypeP_T(!typeP_T)}
+										style={{ color: "#6930c3" }}
+									></i>
+								) : (
+									<i
+										className="fa-solid fa-eye-slash"
+										onClick={() => setTypeP_T(!typeP_T)}
+									></i>
+								)}
+							</span>
+						)}
+						{/* for type toggle end  */}
+					</tr>
+
+					<tr className="required-row">
+						<input
+							type={typeCP_T ? "text" : "password"}
+							required
+							placeholder="Conform Password"
+							name="c_password"
+							value={c_password}
+							onChange={(e) => setC_Password(e.target.value)}
+						/>
+						{/* for type toggle start  */}
+						{c_password && (
+							<span className="eye">
+								{typeCP_T ? (
+									<i
+										className="fa-solid fa-eye"
+										onClick={() => setTypeCP_T(!typeCP_T)}
+										style={{ color: "#6930c3" }}
+									></i>
+								) : (
+									<i
+										className="fa-solid fa-eye-slash"
+										onClick={() => setTypeCP_T(!typeCP_T)}
+									></i>
+								)}
+							</span>
+						)}
+						{/* for type toggle end  */}
+					</tr>
+				</table>
+			</div>
+		</>
+	);
 };
 
 export default SignFieldsPage;
