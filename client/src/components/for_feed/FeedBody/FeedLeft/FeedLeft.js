a3d7e263021c829204cfb21fb1a3937923bc8848
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2022-10-13 21:29:58 +0600

    left-sidebar set-up modify

--- client/src/components/for_feed/FeedBody/FeedLeft/FeedLeft.js
+++ client/src/components/for_feed/FeedBody/FeedLeft/FeedLeft.js
@@ -10,82 +10,82 @@ const FeedLeft = () => {
 	const { currentUser } = GetContextApi();
 
 	// for attach active class
-	const [feed, setFeed] = useState(true);
+	const [settings, setSettings] = useState(false);
+	const [collections, setCollections] = useState(false);
+	const [memories, setMemories] = useState(false);
+	const [adManager, setAdManager] = useState(false);
+	const [people, setPeople] = useState(false);
 	const [page, setPage] = useState(false);
-	const [explore, setExplore] = useState(false);
-	const [bookmark, setBookmark] = useState(false);
-	const [wishlist, setWishlist] = useState(false);
-	const [purchase, setPurchase] = useState(false);
-	const [sitting, setSitting] = useState(false);
+	const [purchased, setPurchased] = useState(false);
 
-	const feedClicked = () => {
-		setFeed(true);
+	const settingsClicked = () => {
+		setSettings(true);
+		setCollections(false);
+		setMemories(false);
+		setAdManager(false);
+		setPeople(false);
 		setPage(false);
-		setExplore(false);
-		setBookmark(false);
-		setWishlist(false);
-		setPurchase(false);
-		setSitting(false);
+		setPurchased(false);
+	};
+
+	const collectionsClicked = () => {
+		setSettings(false);
+		setCollections(true);
+		setMemories(false);
+		setAdManager(false);
+		setPeople(false);
+		setPage(false);
+		setPurchased(false);
+	};
+
+	const memoriesClicked = () => {
+		setSettings(false);
+		setCollections(false);
+		setMemories(true);
+		setAdManager(false);
+		setPeople(false);
+		setPage(false);
+		setPurchased(false);
+	};
+
+	const adManagerClicked = () => {
+		setSettings(false);
+		setCollections(false);
+		setMemories(false);
+		setAdManager(true);
+		setPeople(false);
+		setPage(false);
+		setPurchased(false);
+	};
+
+	const peopleClicked = () => {
+		setSettings(false);
+		setCollections(false);
+		setMemories(false);
+		setAdManager(false);
+		setPeople(true);
+		setPage(false);
+		setPurchased(false);
 	};
 
 	const pageClicked = () => {
-		setFeed(false);
+		setSettings(false);
+		setCollections(false);
+		setMemories(false);
+		setAdManager(false);
+		setPeople(false);
 		setPage(true);
-		setExplore(false);
-		setBookmark(false);
-		setWishlist(false);
-		setPurchase(false);
-		setSitting(false);
+		setPurchased(false);
 	};
 
-	const exploreClicked = () => {
-		setFeed(false);
+	const purchasedClicked = () => {
+		setSettings(false);
+		setCollections(false);
+		setMemories(false);
+		setAdManager(false);
+		setPeople(false);
 		setPage(false);
-		setExplore(true);
-		setBookmark(false);
-		setWishlist(false);
-		setPurchase(false);
-		setSitting(false);
-	};
-
-	const bookmarkClicked = () => {
-		setFeed(false);
-		setPage(false);
-		setExplore(false);
-		setBookmark(true);
-		setWishlist(false);
-		setPurchase(false);
-		setSitting(false);
-	};
-
-	const wishlistClicked = () => {
-		setFeed(false);
-		setPage(false);
-		setExplore(false);
-		setBookmark(false);
-		setWishlist(true);
-		setPurchase(false);
-		setSitting(false);
-	};
-
-	const purchaseClicked = () => {
-		setFeed(false);
-		setPage(false);
-		setExplore(false);
-		setBookmark(false);
-		setWishlist(false);
-		setPurchase(true);
-		setSitting(false);
-	};
-
-	const sittingClicked = () => {
-		setFeed(false);
-		setPage(false);
-		setExplore(false);
-		setBookmark(false);
-		setWishlist(false);
-		setPurchase(false);
-		setSitting(true);
+		setPurchased(true);
 	};
 
 	return (
@@ -105,54 +105,57 @@ const FeedLeft = () => {
 
 				{/* sidebar start  */}
 				<div className="sidebar-container">
-					<span className={feed ? "feed-active" : null} onClick={feedClicked}>
-						<i className="bi bi-rss"></i>
-						<h5>News Feed</h5>
+					<span
+						className={settings ? "settings-active" : null}
+						onClick={settingsClicked}
+					>
+						<img src="/assets/icon/gear.png" alt="settings-icon" />
+						<h5>Settings</h5>
+					</span>
+
+					<span
+						className={collections ? "collections-active" : null}
+						onClick={collectionsClicked}
+					>
+						<img src="/assets/icon/Collection.png" alt="collection-icon" />
+						<h5>Collections</h5>
+					</span>
+
+					<span
+						className={memories ? "memories-active" : null}
+						onClick={memoriesClicked}
+					>
+						<img src="/assets/icon/memories.png" alt="memories-icon" />
+						<h5>Memories</h5>
+					</span>
+
+					<span
+						className={adManager ? "adManager-active" : null}
+						onClick={adManagerClicked}
+					>
+						<img src="/assets/icon/Goal.png" alt="ad-manager-icon" />
+						<h5>AD Manager</h5>
+					</span>
+
+					<span
+						className={people ? "people-active" : null}
+						onClick={peopleClicked}
+					>
+						<img src="/assets/icon/People.png" alt="people-icon" />
+						<h5>People</h5>
 					</span>
 
 					<span className={page ? "page-active" : null} onClick={pageClicked}>
-						<i className="bi bi-paperclip"></i>
-						<h5>Pages</h5>
+						<img src="/assets/icon/page.png" alt="page-icon" />
+						<h5>Page</h5>
 					</span>
 
 					<span
-						className={explore ? "explore-active" : null}
-						onClick={exploreClicked}
+						className={purchased ? "purchased-active" : null}
+						onClick={purchasedClicked}
 					>
-						<i className="bi bi-compass"></i>
-						<h5>Explore</h5>
-					</span>
-
-					<span
-						className={bookmark ? "bookmark-active" : null}
-						onClick={bookmarkClicked}
-					>
-						<i className="bi bi-bookmark-check"></i>
-						<h5>Bookmark</h5>
-					</span>
-
-					<span
-						className={wishlist ? "wishlist-active" : null}
-						onClick={wishlistClicked}
-					>
-						<i className="bi bi-cart4"></i>
-						<h5>Wishlist</h5>
-					</span>
-
-					<span
-						className={purchase ? "purchase-active" : null}
-						onClick={purchaseClicked}
-					>
-						<i className="bi bi-bag-heart"></i>
-						<h5>Purchase</h5>
-					</span>
-
-					<span
-						className={sitting ? "sitting-active" : null}
-						onClick={sittingClicked}
-					>
-						<i className="bi bi-gear"></i>
-						<h5>Settings</h5>
+						<img src="/assets/icon/purchased.png" alt="purchased-icon" />
+						<h5>Purchased</h5>
 					</span>
 				</div>
 				{/*  sidebar end  */}
