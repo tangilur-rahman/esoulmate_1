c1d36cfceed8461949023de3e81d89e548e3a927
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2022-10-14 10:56:40 +0600

    preloader done with forget-password

--- client/src/pages/ForgetPassword/ForgetPassword.js
+++ client/src/pages/ForgetPassword/ForgetPassword.js
@@ -1,21 +1,37 @@
 // external components
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../../components/for_forget_pass/Header/Header";
 
 // internal components
+import PreloaderWithoutAOS from "../../components/Preloader/PreloaderWithoutAOS";
 import "./ForgetPassword.css";
 
 const ForgetPassword = () => {
+	// for waiting for preloader displaying start
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 500);
+	}, []);
+	// for waiting for preloader displaying end
+
 	return (
 		<>
-			<div className="container-fluid p-0">
-				<div className="row m-0">
-					<div className="col p-0">
-						<Header />
-						<Outlet />
+			{isLoading ? (
+				<PreloaderWithoutAOS />
+			) : (
+				<div className="container-fluid p-0">
+					<div className="row m-0">
+						<div className="col p-0">
+							<Header />
+							<Outlet />
+						</div>
 					</div>
 				</div>
-			</div>
+			)}
 		</>
 	);
 };
