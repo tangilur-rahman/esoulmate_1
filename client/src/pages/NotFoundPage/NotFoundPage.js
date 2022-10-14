ab6fc1a119966952fe97b16a68a3aee41274c22d
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2022-10-14 10:49:02 +0600

    preloader done with notFoundPage

--- client/src/pages/NotFoundPage/NotFoundPage.js
+++ client/src/pages/NotFoundPage/NotFoundPage.js
@@ -1,19 +1,40 @@
+// external components
+import { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+
+// internal components
+import PreloaderWithoutAOS from "../../components/Preloader/PreloaderWithoutAOS";
 import "./NotFoundPage.css";
 
 const NotFoundPage = () => {
+	// for waiting for preloader displaying start
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 500);
+	}, []);
+	// for waiting for preloader displaying end
+
 	return (
-		<div className="container-fluid p-0">
-			<div className="row m-0" id="error-row-container">
-				<div className="col p-0" id="error-col-container">
-					<div id="error-link">
-						<NavLink to="/" id="error-navlink" className="hover-link">
-							<span>Go Back To Home</span>
-						</NavLink>
+		<>
+			{isLoading ? (
+				<PreloaderWithoutAOS />
+			) : (
+				<div className="container-fluid p-0">
+					<div className="row m-0" id="error-row-container">
+						<div className="col p-0" id="error-col-container">
+							<div id="error-link">
+								<NavLink to="/" id="error-navlink" className="hover-link">
+									<span>Go Back To Home</span>
+								</NavLink>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-		</div>
+			)}
+		</>
 	);
 };
 
