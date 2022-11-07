1935179c67e129535968d1d78ec89b1bc34d209b
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2022-11-07 16:52:56 +0600

    work & edu wrapper responsive done

--- client/src/components/for_profile/TabContents/AboutTab/AboutTab.js
+++ client/src/components/for_profile/TabContents/AboutTab/AboutTab.js
@@ -16,34 +16,36 @@ const AboutTab = ({ getProfile }) => {
 		<>
 			<div className="row m-0 about-tab-container">
 				{/* left-about-container start  */}
-				<div className="col-lg-3 col-12 p-0 left-about-con">
-					<h4>About</h4>
-					<ul>
-						<li
-							id={selectedTab === "Work & Education" ? "active" : ""}
-							onClick={() => setSelectedTab("Work & Education")}
-						>
-							Work & Education
-						</li>
-						<li
-							id={selectedTab === "Location" ? "active" : ""}
-							onClick={() => setSelectedTab("Location")}
-						>
-							Location
-						</li>
-						<li
-							id={selectedTab === "Contact & Basic info" ? "active" : ""}
-							onClick={() => setSelectedTab("Contact & Basic info")}
-						>
-							Contact & Basic info
-						</li>
-						<li
-							id={selectedTab === "Details about you" ? "active" : ""}
-							onClick={() => setSelectedTab("Details about you")}
-						>
-							Details about you
-						</li>
-					</ul>
+				<div className="col-lg-3 col-12 p-0 ">
+					<div className="left-about-con">
+						<h4>About</h4>
+						<ul>
+							<li
+								id={selectedTab === "Work & Education" ? "active" : ""}
+								onClick={() => setSelectedTab("Work & Education")}
+							>
+								Work & Education
+							</li>
+							<li
+								id={selectedTab === "Location" ? "active" : ""}
+								onClick={() => setSelectedTab("Location")}
+							>
+								Location
+							</li>
+							<li
+								id={selectedTab === "Contact & Basic info" ? "active" : ""}
+								onClick={() => setSelectedTab("Contact & Basic info")}
+							>
+								Contact & Basic info
+							</li>
+							<li
+								id={selectedTab === "Details about you" ? "active" : ""}
+								onClick={() => setSelectedTab("Details about you")}
+							>
+								Details about you
+							</li>
+						</ul>
+					</div>
 				</div>
 				{/* left-about-container end  */}
 
