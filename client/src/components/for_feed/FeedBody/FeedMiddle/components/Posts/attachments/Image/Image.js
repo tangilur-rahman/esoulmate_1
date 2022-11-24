48e4dd1753c6b9ee46f401c02b3aa8c7455d98d1
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2022-11-24 20:33:17 +0600

    displaying image done for now

--- client/src/components/for_feed/FeedBody/FeedMiddle/components/Posts/attachments/Image/Image.js
+++ client/src/components/for_feed/FeedBody/FeedMiddle/components/Posts/attachments/Image/Image.js
@@ -1,4 +1,6 @@
 // external components
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 
 // internal components
 import "./Image.css";
@@ -7,16 +9,83 @@ const Image = ({ image }) => {
 	return (
 		<>
 			<div className="img-container">
-				{image.map((value, index) => {
-					return (
-						<img
-							src={`/uploads/attachments/${value}`}
-							alt="post-img"
-							className="img-fluid for-image"
-							key={index}
-						/>
-					);
-				})}
+				{image.length < 3 && (
+					<PhotoProvider>
+						{image.length > 0 &&
+							image.map((value, index) => {
+								return (
+									<PhotoView key={index} src={`/uploads/attachments/${value}`}>
+										{
+											<img
+												src={`/uploads/attachments/${value}`}
+												alt="post-img"
+												className="img-fluid"
+												id={
+													(index === 0 ? "single-image" : "") ||
+													(index === 1 ? "double-images" : "")
+												}
+											/>
+										}
+									</PhotoView>
+								);
+							})}
+					</PhotoProvider>
+				)}
+
+				{image.length === 3 && (
+					<div className="when-third-images">
+						<PhotoProvider>
+							{image.map((value, index) => {
+								return (
+									<PhotoView key={index} src={`/uploads/attachments/${value}`}>
+										{
+											<img
+												src={`/uploads/attachments/${value}`}
+												alt="post-img"
+												className="img-fluid"
+												id={
+													(index === 0 ? "first-image" : "") ||
+													(index === 1 ? "second-image" : "") ||
+													(index === 2 ? "third-image" : "")
+												}
+											/>
+										}
+									</PhotoView>
+								);
+							})}
+						</PhotoProvider>
+					</div>
+				)}
+
+				{image.length >= 4 && (
+					<div className="when-forth-images">
+						<PhotoProvider>
+							{image.map((value, index) => {
+								return (
+									<PhotoView key={index} src={`/uploads/attachments/${value}`}>
+										{index <= 3 && (
+											<img
+												src={`/uploads/attachments/${value}`}
+												alt="post-img"
+												className={
+													index === 3 && image.length > 4 ? "when-counter" : ""
+												}
+												id={
+													(index === 0 ? "first-image" : "") ||
+													(index === 1 ? "second-image" : "") ||
+													(index === 2 ? "third-image" : "") ||
+													(index === 3 ? "forth-image" : "")
+												}
+											/>
+										)}
+									</PhotoView>
+								);
+							})}
+						</PhotoProvider>
+
+						{image.length > 4 && <h5 id="counter">{`+${image.length - 4}`}</h5>}
+					</div>
+				)}
 			</div>
 		</>
 	);
