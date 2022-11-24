d9619c13c244595e7feb64b208cf49676b72916c
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2022-11-24 21:43:05 +0600

    displaying double video done for now

--- client/src/components/for_feed/FeedBody/FeedMiddle/components/Posts/attachments/Video/Video.js
+++ client/src/components/for_feed/FeedBody/FeedMiddle/components/Posts/attachments/Video/Video.js
@@ -5,21 +5,23 @@ import "./Video.css";
 
 const Video = ({ video }) => {
 	return (
-		<>
-			{video.map((value, index) => {
-				return (
-					<video
-						src={`/uploads/attachments/${value}`}
-						controls="controls"
-						controlsList="nodownload"
-						autoPlay={true}
-						muted
-						className="for-video"
-						key={index}
-					/>
-				);
-			})}
-		</>
+		<div className="video-container">
+			{video.length > 0 &&
+				video.length < 3 &&
+				video.map((value, index) => {
+					return (
+						<video
+							src={`/uploads/attachments/${value}`}
+							controls="controls"
+							controlsList="nodownload"
+							autoPlay={true}
+							muted
+							key={index}
+							id={value.length === 1 ? "single-video" : "double-videos"}
+						/>
+					);
+				})}
+		</div>
 	);
 };
 
