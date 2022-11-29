c0aedfc32baa6b9292f8ffee6483ac4feda71df7
Tangilur Rahman <mohammadtangilurrahaman@gmail.com>
2022-11-29 22:52:56 +0600

    multiple videos displaying almost done

--- client/src/components/for_feed/FeedBody/FeedMiddle/components/Posts/attachments/Video/Video.js
+++ client/src/components/for_feed/FeedBody/FeedMiddle/components/Posts/attachments/Video/Video.js
@@ -21,6 +21,55 @@ const Video = ({ video }) => {
 						/>
 					);
 				})}
+
+			{video.length === 3 && (
+				<div className="when-third-videos">
+					{video.map((value, index) => {
+						return (
+							<video
+								src={`/uploads/attachments/${value}`}
+								controls="controls"
+								controlsList="nodownload"
+								autoPlay={true}
+								muted
+								key={index}
+								id={
+									(index === 0 ? "first-video" : "") ||
+									(index === 1 ? "second-video" : "") ||
+									(index === 2 ? "third-video" : "")
+								}
+							/>
+						);
+					})}
+				</div>
+			)}
+
+			{video.length >= 4 && (
+				<div className="when-forth-videos">
+					{video.map((value, index) => {
+						return (
+							<video
+								src={`/uploads/attachments/${value}`}
+								controls="controls"
+								controlsList="nodownload"
+								autoPlay={true}
+								muted
+								key={index}
+								className={
+									index === 3 && video.length > 4 ? "when-counter" : ""
+								}
+								id={
+									(index === 0 ? "first-video" : "") ||
+									(index === 1 ? "second-video" : "") ||
+									(index === 2 ? "third-video" : "") ||
+									(index === 3 ? "forth-video" : "")
+								}
+							/>
+						);
+					})}
+					{video.length > 4 && <h5 id="counter">{`+${video.length - 4}`}</h5>}
+				</div>
+			)}
 		</div>
 	);
 };
